8fd6d32fbff280f65c8b65ec397257ec5c464f4f
Devlin Liles <Devlin@devlinliles.com>
2013-10-07 19:42:51 -0500

    Initial Feature Work

---
+++ Highway/test/Highway.Data.EntityFramework.Tests/Queries/Given_a_Query_With_Deep_Graph_Includes.cs
@@ -0,0 +1,29 @@
+﻿using Highway.Data.Tests.TestDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Highway.Data.EntityFramework.Tests.UnitTests
+{
+    [TestClass]
+    public class Given_a_Query_With_Deep_Graph_Includes
+    {
+        [TestMethod]
+        public void ShouldAllowMultipleLevelIncludes()
+        {
+            //arrange 
+            var fooQuery = new FindFooWithGraph();
+
+            //act
+
+            //assert
+
+        }
+    }
+
+    public class FindFooWithGraph : Query<Foo>
+    {
+        public FindFooWithGraph()
+        {
+            ContextQuery = c => c.AsQueryable<Foo>().IncludeMany(x => x.Bars).ThenInclude(x => x.Qux);
+        }
+    }
+}
