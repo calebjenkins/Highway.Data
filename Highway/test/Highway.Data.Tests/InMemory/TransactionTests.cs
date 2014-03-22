1261f332f4a3f1261281b374c26dfc63e947102a
Devlin Liles <Devlin@devlinliles.com>
2014-03-22 10:09:49 -0500

    Adding branch and base test

--- Highway/test/Highway.Data.Tests/InMemory/TransactionTests.cs
+++ Highway/test/Highway.Data.Tests/InMemory/TransactionTests.cs
@@ -1,7 +1,28 @@
-﻿namespace Highway.Data.Tests.InMemory
+﻿using System.Linq;
+using Highway.Data.Contexts;
+using Highway.Data.Tests.TestDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Highway.Data.Tests.InMemory
 {
+    [TestClass]
     public class TransactionTests
     {
-         
+        [TestMethod]
+        public void ShouldNotReturnModifiedObject()
+        {
+            //Arrange
+            var context = new InMemoryDataContext();
+            context.Add(new Foo { Name = "Testing"});
+            context.Commit();
+            
+            //Act
+            var foo = context.AsQueryable<Foo>().SingleOrDefault(x => x.Name == "Testing");
+            foo.Name = "Should Matter";
+
+            //Assert
+            var fooTwo = context.AsQueryable<Foo>().SingleOrDefault(x => x.Name == "Testing");
+            Assert.IsNotNull(fooTwo);
+        }
     }
 }
