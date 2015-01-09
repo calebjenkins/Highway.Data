c566b2975e69109e5f6fcf0448ac309b3db86f36
Devlin Liles <dliles@eprod.com>
2015-01-09 10:07:30 -0600

    Adding the inital pass at highway 6.0

--- Highway/test/Highway.Data.Tests/TestDomain/Foo.cs
+++ Highway/test/Highway.Data.Tests/TestDomain/Foo.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
 
 #endregion
 
@@ -9,17 +11,14 @@ namespace Highway.Data.Tests.TestDomain
 {
     public class Foo : IIdentifiable<int>
     {
-        [StoredProcedureAttributes.Name("testName")]
         public virtual string Name { get; set; }
 
-        [StoredProcedureAttributes.Name("testAddress")]
         public virtual string Address { get; set; }
 
         public Bar Bar { get; set; }
 
         public ICollection<Bar> Bars { get; set; }
 
-        [StoredProcedureAttributes.Name("testID")]
         public virtual int Id { get; set; }
 
 
